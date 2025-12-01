b1b61d220dcbba08928af0cec6e5b782cb0139f9
iuliavintan <vintaniulia>
2025-12-01 11:52:11 +0200

    first build and message

---
+++ build/CMakeFiles/joc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joc.dir/main.cpp.o"
+  "CMakeFiles/joc.dir/main.cpp.o.d"
+  "joc"
+  "joc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -3,5 +3,16 @@
 
 int main(void){
 	
+	std::cout<<"Let's start!"<<std::endl;
+	InitWindow(750, 750, "SnakeySnake");
+
+	while(!WindowShouldClose()){
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawText("Let's start!", 300, 350, 30, RAYWHITE);
+		EndDrawing();
+	}
+
+	CloseWindow();
 	return 0;
 }
